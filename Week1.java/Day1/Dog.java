c3ed9b8f0914526d433a19b149bc4b728cd924e2
Sidhardha36 <varmasidhardha2607@gmail.com>
2026-04-09 08:47:51 +0530

    Week1 Day1:Updated the code

--- Week1.java/Day1/Dog.java
+++ Week1.java/Day1/Dog.java
@@ -6,19 +6,6 @@ public class Dog {
         System.out.println(name+" is a "+ breed);
     }
 
-
-
-    public class Main {
-
-        Dog d1=new Dog();
-
-        public Main() {
-        }
-
-        
-        d1.name="Jamie";
-        d1.breed="German shepherd";
-
-        d1.display();
-    }
 }
+
+    
---
+++ Week1.java/Day1/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+        Dog d1=new Dog();
+        
+        d1.name="Jamie";
+        d1.breed="German shepherd";
+
+        d1.display();
+}
